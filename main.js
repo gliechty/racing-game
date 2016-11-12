8c6e4ec3c7a02136ac3e43e2eae66efaf385eb24
gliechty <guy.liechty@gmail.com>
2016-11-12 15:24:35 -0700

    linked game to landing page, created game page background, set up divs for players

---
+++ main.js
@@ -0,0 +1,17 @@
+//set event listeners
+
+//set var for num of clicks pl1/pl2
+
+//set function to move plr positions, and add to #clicks
+
+//function to determine win @threshold (determine #clicks needed )
+var isWin = function (){
+	if (pl1Clicks === 10) {
+		alert("Player 1 Wins!!");
+	} else if (pl2Clicks == 10) {
+		alert("player 2 Wins!!");
+	} else{
+		return;
+	}
+	// code for refreshing page
+}
